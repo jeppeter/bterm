d2565598d5004d3662c7a5db72e8f94e4615fb56
jeppeter <jeppeter@gmail.com>
2023-05-11 16:04:01 +0800

    coding for format_bogl_level

--- bogl-debug.c
+++ bogl-debug.c
@@ -3,11 +3,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static int st_bogl_debug_level = 0;
+static int st_bogl_debug_level = BOGL_ERROR;
 static FILE* st_bogl_debug_fp = NULL;
 
 
+int bolg_output_init(void)
+{
+	static int st_bogl_debug_inited = 0;
+	char* envstr=NULL;
+	if (st_bogl_debug_inited == 0) {
+		st_bogl_debug_inited = 1;
+		envstr = getenv("BTERM_DEBUG_LEVEL");
+		if (envstr != NULL) {
+			st_bogl_debug_level = atoi(envstr);
+		}
+
+		envstr = getenv("BTERM_DEBUG_FILE");
+		if (envstr != NULL) {
+			st_bogl_debug_fp = fopen(envstr,"a+");
+		}
+	}
+	return 0;
+}
+
+
+
+int format_bogl_level(char** ppstr, int *psize,int level, char* file,int lineno,const char* fmt,va_list ap)
+{
+	char* curstr=NULL;
+	char* pretstr = NULL;
+	int retsize=0;
+	int retlen = 0;
+	int ret;
+	if (fmt == NULL) {
+		if (ppstr && *ppstr) {
+			free(*ppstr);
+			*ppstr = NULL;
+		}
+
+		if (psize) {
+			*psize = 0;
+		}
+
+		return 0;
+	}
+
+	if (ppstr == NULL || psize == NULL) {
+		ret = -EINVAL;
+		goto fail;
+	}
+
+
+
+
+	return retlen;
+fail:
+	if (curstr) {
+		free(curstr);
+	}
+	curstr = NULL;
+	errno = ret;
+	return ret;
+}
+
+void bogl_output_buffer_debug(int level,char* file, int lineno,void* pbuf, int buflen,char* fmt, ...)
+{
+	bolg_output_init();
+	if (st_bogl_debug_fp != NULL && level <= st_bogl_debug_level) {
+
+	}
+	return
+}
+
 void bogl_output_debug(int level,char* file, int lineno,char* fmt, ...)
 {
+	bolg_output_init();
+	if (st_bogl_debug_fp != NULL && level <= st_bogl_debug_level) {
+		
+	}
 	return;
 }
--- bogl-debug.h
+++ bogl-debug.h
@@ -1,10 +1,20 @@
 #ifndef __BOGL_DEBUG_H_3DE65707D70ECCA3A4FBAAC6E9224053__
 #define __BOGL_DEBUG_H_3DE65707D70ECCA3A4FBAAC6E9224053__
 
+
+
+#define  BOGL_TRACE          4
+#define  BOGL_DEBUG          3
+#define  BOGL_INFO           2
+#define  BOGL_WARN           1
+#define  BOGL_ERROR          0
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus*/
 
+void bogl_output_buffer_debug(int level,char* file, int lineno,void* pbuf, int buflen,char* fmt, ...);
+void bogl_output_debug(int level,char* file, int lineno,char* fmt, ...);
 
 #ifdef __cplusplus
 };
